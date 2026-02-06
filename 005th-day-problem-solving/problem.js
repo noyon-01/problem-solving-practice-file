921e16ba39d3513197d6fc35e03030bc80b988d2
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-06 20:32:44 +0600

    5th day problem solving is completed

--- 005th-day-problem-solving/problem.js
+++ 005th-day-problem-solving/problem.js
@@ -1,30 +1,31 @@
+// !JavaScript Problem Solving - 01! //
 /**
  * Multiply
  * This code does not execute properly. Try to figure out why.
  */
 
-function multiply(a, b) {
-  return a * b;
-}
+// function multiply(a, b) {
+//   return a * b;
+// }
+// multiply(5, 6);
 
-multiply(5, 6);
-
+// !JavaScript Problem Solving - 02! //
 /**
  * Return Negative
- * 
  * In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
-Examples
-makeNegative(1);    // return -1
-makeNegative(-5);   // return -5
-makeNegative(0);    // return 0
-makeNegative(0.12); // return -0.12
-Notes
-The number can be negative already, in which case no change is required.
-Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+ * Examples
+   makeNegative(1);    // return -1
+   makeNegative(-5);   // return -5
+   makeNegative(0);    // return 0
+   makeNegative(0.12); // return -0.12
+
+ * Notes
+   The number can be negative already, in which case no change is required.
+   Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
  * 
  */
 
-function makeNegative(num) {
-  return num > 0 ? num * -1 : num;
-}
+// function makeNegative(num) {
+//   return num > 0 ? num * -1 : num;
+// }
