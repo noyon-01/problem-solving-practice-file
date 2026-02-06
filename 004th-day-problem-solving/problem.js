e0d295819c18efc65f81bfa7157a49a424994f82
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-06 20:30:27 +0600

    4th day problem solving is completed

--- 004th-day-problem-solving/problem.js
+++ 004th-day-problem-solving/problem.js
@@ -1,3 +1,4 @@
+// !JavaScript Problem Solving - 01! //
 /**
  * Convert a String to a Number! 
  * Description
@@ -5,126 +6,93 @@
 
  * Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
-Examples
-"1234" --> 1234
-"605"  --> 605
-"1405" --> 1405
-"-7" --> -7
+ * Examples
+   "1234" --> 1234
+   "605"  --> 605
+   "1405" --> 1405
+   "-7" --> -7
  */
 
-// const stringToNumber = function(str){
+// const stringToNumber = function (str) {
 //   let number = +str;
 //   return number;
-// }
+// };
 
+// !JavaScript Problem Solving - 02! //
 /**
  * Remove String Spaces
  * Write a function that removes the spaces from the string, then return the resultant string.
 
-Examples (Input -> Output):
-
-"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
-"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
-"8aaaaa dddd r     " -> "8aaaaaddddr"
+ * Examples (Input -> Output):
+   "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+   "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+   "8aaaaa dddd r     " -> "8aaaaaddddr"
  */
 
-function noSpace(x) {
-  let noSpace = x.split(" ").join("");
-  return noSpace;
-}
+// function noSpace(x) {
+//   let noSpace = x.split(" ").join("");
+//   return noSpace;
+// }
 
-/**
+// !JavaScript Problem Solving - 03! //
+/** 
  * Remove First and Last Character
- * 
- * Remove First and Last Character
-Task
-Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+ * Task
+ * Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
 
-Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+ * Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
 
-Examples
-'eloquent' --> 'loquen'
-'country'  --> 'ountr' 
-'person'   --> 'erso'
-'ab'       --> '' (empty string)
-'xyz'      --> 'y'
-Requirements
-The input string will always have at least 2 characters
-For strings with exactly 2 characters, return an empty string
-For strings with 3 or more characters, remove the first and last character
-The function should handle strings containing letters, numbers, and special characters
-Test Cases
-Your solution will be tested against:
+ * Examples
+   'eloquent' --> 'loquen'
+   'country'  --> 'ountr' 
+   'person'   --> 'erso'
+   'ab'       --> '' (empty string)
+   'xyz'      --> 'y'
 
-Basic functionality with common words
-Edge cases with 2-character and 3-character strings
-Strings containing numbers and special characters
-Random test cases of varying lengths
+ * Requirements
+   The input string will always have at least 2 characters
+   For strings with exactly 2 characters, return an empty string
+   For strings with 3 or more characters, remove the first and last character
+   The function should handle strings containing letters, numbers, and special characters.
+
+ * Test Cases
+ * Your solution will be tested against:
+
+ * Basic functionality with common words
+   Edge cases with 2-character and 3-character strings
+   Strings containing numbers and special characters
+   Random test cases of varying lengths
  */
 
-function removeChar(str) {
-  //You got this!
-  return str.slice(1, -1);
-}
+// function removeChar(str) {
+//   return str.slice(1, -1);
+// }
 
+// !JavaScript Problem Solving - 04! //
 /**
  * Reversed Strings
- * 
  * Complete the solution so that it reverses the string passed into it.
-
-'world'  =>  'dlrow'
-'word'   =>  'drow'
+   'world'  =>  'dlrow'
+   'word'   =>  'drow'
  */
 
-function solution(str) {
-  return str.split("").reverse().join("");
-}
+// function solution(str) {
+//   return str.split("").reverse().join("");
+// }
 
+// !JavaScript Problem Solving - 05! //
 /**
  * Convert a Number to a String!
- * 
  * We need a function that can transform a number (integer) into a string.
 
-What ways of achieving this do you know?
+ * What ways of achieving this do you know?
 
-Examples (input --> output):
-123  --> "123"
-999  --> "999"
--100 --> "-100"
+ * Examples (input --> output):
+   123  --> "123"
+   999  --> "999"
+   -100 --> "-100"
  */
 
-function numberToString(num) {
-  // Return a string of the number here!
-  return num.toString();
-}
-
-/**
- * Multiply
- * This code does not execute properly. Try to figure out why.
- */
-
-function multiply(a, b) {
-  return a * b;
-}
-
-multiply(5, 6);
-
-/**
- * Return Negative
- * 
- * In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
-
-Examples
-makeNegative(1);    // return -1
-makeNegative(-5);   // return -5
-makeNegative(0);    // return 0
-makeNegative(0.12); // return -0.12
-Notes
-The number can be negative already, in which case no change is required.
-Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
- * 
- */
-
-function makeNegative(num) {
-  return num > 0 ? num * -1 : num;
-}
+// function numberToString(num) {
+//   return num.toString();
+// }
